37cbd8c62b7ace30346d79a9436f82037dc9db25
Ingy döt Net <ingy@ingy.net>
2014-08-16 03:51:58 -0700

    CPAN Release 0.22
    - Eliminate spurious trailing whitespace

--- lib/orz.pm
+++ lib/orz.pm
@@ -1,12 +1,12 @@
 use strict; use warnings;
 package orz;
-our $VERSION = '0.21';
+our $VERSION = '0.22';
 
 use Module::Compile -base;
 
 sub pmc_compile {
     s/^/# /gm;
-    return "# orz...\n$_"; 
+    return "# orz...\n$_";
 }
 
 1;
